4d2927fb33f4d87a5925cc52c179902636589816
iem <iem@imac-de-iut-3.local>
2016-02-03 12:06:00 +0100

    Ajout Model

---
+++ MyNotes/Category.swift
@@ -0,0 +1,17 @@
+//
+//  Category.swift
+//  MyNotes
+//
+//  Created by iem on 03/02/2016.
+//  Copyright © 2016 iem. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+class Category: NSManagedObject {
+
+// Insert code here to add functionality to your managed object subclass
+
+}

6717a4e2e696218cbe946442f149dc587e6b07a6
Marco Loiodice <mloiodice13@gmail.com>
2016-02-07 17:29:35 +0100

    make CategoryViewController

--- MyNotes/CategoryViewController.swift
+++ MyNotes/CategoryViewController.swift
@@ -13,11 +13,23 @@ class CategoryViewController: UIViewController {
     
     @IBOutlet weak var tableView: UITableView!
     
+    var appDelegate: AppDelegate{
+        return UIApplication.sharedApplication().delegate as! AppDelegate
+    }
+    
+    var categories: [Category]{
+        return CategoryManager().fetchCategory()!
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        let categoryManager=CategoryManager()
+        categoryManager.createCategory("Toto")
+        categoryManager.createCategory("Titi")
+        categoryManager.createCategory("Tutu")
+        categoryManager.createCategory("Tata")
+        
+        categoryManager.appDelegate.saveContext()
     }
 
     override func didReceiveMemoryWarning() {
@@ -26,14 +38,23 @@ class CategoryViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    //MARK: UITableViewDataSource
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return categories.count
     }
-    */
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let category=categories[indexPath.row]
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell")
+        cell?.textLabel?.text=category.name
+        
+        return cell!
+    }
+    
+    func tableView(tableView: UITableView, didDeselectRowAtIndexPath indexPath: NSIndexPath) {
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+    }
 
 }

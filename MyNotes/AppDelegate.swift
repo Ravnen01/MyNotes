d4eb59bd77e09572e9a301269e95eace4f180633
Marco L <mloiodice13@gmail.com>
2016-02-10 09:22:33 +0100

    update categories model and make auto migration CoreData

--- MyNotes/AppDelegate.swift
+++ MyNotes/AppDelegate.swift
@@ -65,7 +65,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         let url = self.applicationDocumentsDirectory.URLByAppendingPathComponent("SingleViewCoreData.sqlite")
         var failureReason = "There was an error creating or loading the application's saved data."
         do {
-            try coordinator.addPersistentStoreWithType(NSSQLiteStoreType, configuration: nil, URL: url, options: nil)
+            
+            //add options for automatically migration
+            let options = [NSMigratePersistentStoresAutomaticallyOption : true, NSInferMappingModelAutomaticallyOption : true]
+            
+            try coordinator.addPersistentStoreWithType(NSSQLiteStoreType, configuration: nil, URL: url, options: options)
         } catch {
             // Report any error we got.
             var dict = [String: AnyObject]()
